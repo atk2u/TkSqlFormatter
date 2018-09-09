7e12ef8502e73aec4473125bda841a606c1cf078
taylorkelseydude <taylorkelseydude@gmail.com>
2018-09-08 20:48:38 -0500

    Add project files.

---
+++ TkSqlFormatter/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TkSqlFormatter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlFormatter formatter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            formatter = new SqlFormatter();
+        }
+
+        private void btnFormat_Click(object sender, RoutedEventArgs e)
+        {
+            SqlOutput.Text = formatter.FormatSql(SqlInput.Text);
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(formatter.AboutMessage(), "About", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ TkSqlFormatter/SqlFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace TkSqlFormatter
+{
+    internal class SqlFormatter
+    {
+        internal string FormatSql(string sqlInput)
+        {
+            string retVal = string.Empty;
+
+            return retVal;
+        }
+
+        internal string AboutMessage()
+        {
+            return "Developed by: Aaron Taylor Kelsey";
+        }
+    }
+}
